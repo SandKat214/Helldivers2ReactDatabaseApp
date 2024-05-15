e3d5341da3061fe6812a56b2c3c9ace400d0bc41
Katherine Sandeen <sandeenk@classwork.engr.oregonstate.edu>
2024-05-15 10:13:11 -0700

    teamPlayer table legend

--- App/frontend/src/pages/teamPlayers/sections/TeamPlayersTable.jsx
+++ App/frontend/src/pages/teamPlayers/sections/TeamPlayersTable.jsx
@@ -1,7 +1,6 @@
 import {
   Avatar,
   Box,
-  Heading,
   HStack,
   IconButton,
   Table,
@@ -40,85 +39,84 @@ const TeamPlayersTable = ({ team, status, setStatus }) => {
   }, []);
 
   return (
-    <VStack maxW="1200px" w="1300px" gap={20} alignItems="center" mb={20}>
-      <VStack w="1300px" gap={5}>
-        <Text color="white" fontSize="large">Teams can recruit up to 4 players...</Text>
-        <Box
-          backgroundColor="background.200"
-          maxH="500px"
-          overflowY="auto"
-          borderRadius="md"
-          p={1}
-          boxShadow="0px 2px 12px rgba(229, 62, 62, 0.3)"
-          w="70%"
-        >
-          <TableContainer>
-            <Table variant="unstyled">
-              <Thead>
-                <Tr>
-                  <Th borderColor="transparent" color="white">
-                    Profile Photo
-                  </Th>
-                  <Th borderColor="transparent" color="white">
-                    Name
-                  </Th>
-                  <Th borderColor="transparent" color="white">
-                    Username
-                  </Th>
-                  <Th borderColor="transparent" color="white">
-                    Level
-                  </Th>
-                  <Th borderColor="transparent" color="white">
-                    Age
-                  </Th>
-                  <Th borderColor="transparent" color="white" isNumeric>
-                    Actions
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {teamMembers.map((teamPlayer, index) => {
-                  return (
-                    <Tr
-                      key={teamPlayer.playerID}
-                      color="white"
-                      backgroundColor={
-                        index % 2 === 0 ? "background.600" : "transparent"
-                      }
-                      transition="all 0.3s"
-                      _hover={{
-                        backgroundColor: "#0B111D",
-                      }}
-                      h={24}
-                    >
-                      <Td borderColor="transparent">
-                        <Avatar src="https://www.nikkoindustries.com/cdn/shop/files/Image-Render.000_e98f6660-9faa-47ff-8d91-8df35964c70e_946x946.png?v=1708613784" />
-                      </Td>
-                      <Td borderColor="transparent">{teamPlayer.name}</Td>
-                      <Td borderColor="transparent">{teamPlayer.username}</Td>
-                      <Td borderColor="transparent">{teamPlayer.level}</Td>
-                      <Td borderColor="transparent">{teamPlayer.age}</Td>
-                      <Td>
-                        <HStack justifyContent="flex-end">
-                          <Tooltip label="Remove from Team" placement="top">
-                            <IconButton
-                              colorScheme="red"
-                              color="white"
-                              aria-label="Edit button"
-                              icon={<FaTrash />}
-                              size="sm"
-                            />
-                          </Tooltip>
-                        </HStack>
-                      </Td>
-                    </Tr>
-                  );
-                })}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-      </VStack>
+    <VStack w="1300px" gap={5}>
+      <Text color="white" fontSize="large">Teams can register up to 4 players...</Text>
+      <Box
+        backgroundColor="background.200"
+        maxH="500px"
+        overflowY="auto"
+        borderRadius="md"
+        p={1}
+        boxShadow="0px 2px 12px rgba(229, 62, 62, 0.3)"
+        w="70%"
+      >
+        <Text color="red.500" pt={2} pl={3} textTransform="uppercase" fontWeight="bold" fontSize="md">Current recruits:</Text>
+        <TableContainer>
+          <Table variant="unstyled">
+            <Thead>
+              <Tr>
+                <Th borderColor="transparent" color="white">
+                  Profile Photo
+                </Th>
+                <Th borderColor="transparent" color="white">
+                  Name
+                </Th>
+                <Th borderColor="transparent" color="white">
+                  Username
+                </Th>
+                <Th borderColor="transparent" color="white">
+                  Level
+                </Th>
+                <Th borderColor="transparent" color="white">
+                  Age
+                </Th>
+                <Th borderColor="transparent" color="white" isNumeric>
+                  Actions
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {teamMembers.map((teamPlayer, index) => {
+                return (
+                  <Tr
+                    key={teamPlayer.playerID}
+                    color="white"
+                    backgroundColor={
+                      index % 2 === 0 ? "background.600" : "transparent"
+                    }
+                    transition="all 0.3s"
+                    _hover={{
+                      backgroundColor: "#0B111D",
+                    }}
+                    h={24}
+                  >
+                    <Td borderColor="transparent">
+                      <Avatar src="https://www.nikkoindustries.com/cdn/shop/files/Image-Render.000_e98f6660-9faa-47ff-8d91-8df35964c70e_946x946.png?v=1708613784" />
+                    </Td>
+                    <Td borderColor="transparent">{teamPlayer.name}</Td>
+                    <Td borderColor="transparent">{teamPlayer.username}</Td>
+                    <Td borderColor="transparent">{teamPlayer.level}</Td>
+                    <Td borderColor="transparent">{teamPlayer.age}</Td>
+                    <Td>
+                      <HStack justifyContent="flex-end">
+                        <Tooltip label="Remove from Team" placement="top">
+                          <IconButton
+                            colorScheme="red"
+                            color="white"
+                            aria-label="Edit button"
+                            icon={<FaTrash />}
+                            size="sm"
+                          />
+                        </Tooltip>
+                      </HStack>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
     </VStack>
   );
 };
